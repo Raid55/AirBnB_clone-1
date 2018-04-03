32eb388193154aac9a41e418eeee5608ef8974e3
Raid55 <boutboulnicholas@gmail.com>
2018-04-03 20:40:36 +0000

    fixed small typo

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,6 +7,7 @@ import os
 
 env.hosts = ['34.227.229.159', '34.229.125.244']
 
+
 def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
@@ -16,10 +17,13 @@ def do_deploy(archive_path):
         run("mkdir -p /data/web_static/releases/{}".format(archive[:-4]))
         run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
             .format(archive, archive[:-4]))
-        sudo("rm /tmp/{}".format(archive))
-        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/"
+        run("rm /tmp/{}".format(archive))
+        run(
+            "mv /data/web_static/releases/{}/web_static/* \
+             /data/web_static/releases/{}/"
             .format(archive[:-4], archive[:-4]))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(archive[:-4]))
+        run("rm -rf /data/web_static/releases/{}/web_static"
+            .format(archive[:-4]))
         run("rm -rf /data/web_static/current")
         run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
             .format(archive[:-4]))
