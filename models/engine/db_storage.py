cbe0ffb76574ae3208d8287196ccf2cbd5de13ea
Raid55 <boutboulnicholas@gmail.com>
2018-04-17 22:11:50 +0000

    pep8

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -18,7 +18,6 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import scoped_session
 
 
-
 class DBStorage:
     """
         a connection for the database
@@ -26,15 +25,17 @@ class DBStorage:
     __engine = None
     __session = None
 
-
     def __init__(self):
         self.__engine = create_engine(
-        'mysql+mysqldb://{}:{}@{}/{}'.format(
-            getenv("HBNB_MYSQL_USER"),
-            getenv("HBNB_MYSQL_PWD"),
-            getenv("HBNB_MYSQL_HOST"),
-            getenv("HBNB_MYSQL_DB")),
-        pool_pre_ping=True, echo=False)
+            'mysql+mysqldb://{}:{}@{}/{}'.format(
+                getenv("HBNB_MYSQL_USER"),
+                getenv("HBNB_MYSQL_PWD"),
+                getenv("HBNB_MYSQL_HOST"),
+                getenv("HBNB_MYSQL_DB")
+            ),
+            pool_pre_ping=True,
+            echo=False
+        )
 
         if getenv("HBNB_ENV") == 'test':
             Base.metadata.drop_all(self.__engine)
