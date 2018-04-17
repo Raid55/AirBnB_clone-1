dda31f00531bf2a08edbb1126c6868afd55d5d7c
Raid55 <boutboulnicholas@gmail.com>
2018-04-17 20:57:56 +0000

    uppercase

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -17,7 +17,7 @@ def just_hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_with_text(text):
-    return 'c {}'.format(text.replace('_', ' '))
+    return 'C {}'.format(text.replace('_', ' '))
 
 
 if __name__ == "__main__":
